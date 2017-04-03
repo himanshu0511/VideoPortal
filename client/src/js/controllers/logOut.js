71d232331069a838527c275205032d62a3239f79
Himanshu <himanshu511@outlook.com>
2017-04-04 01:54:23 +0530

    add style to login

--- client/src/js/controllers/logOut.js
+++ client/src/js/controllers/logOut.js
@@ -1,5 +1,4 @@
 angular.module('videoPortal').controller('LogOutController', ['UserService', '$location', function(UserService, $location){
-  debugger;
   return UserService.logOut().then(function(){
     $location.path('/');
   })
